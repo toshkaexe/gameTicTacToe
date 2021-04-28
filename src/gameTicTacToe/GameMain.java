41ef942dc7749818ca03117060692ebaa7f08eb4
Anton Zeltser <anton.zeltser@gmail.com>
2021-04-28 11:30:50 +0200

    Update GameMain.java

--- src/gameTicTacToe/GameMain.java
+++ src/gameTicTacToe/GameMain.java
@@ -8,11 +8,8 @@ public class GameMain {
 
 	    try (Scanner in = new Scanner(System.in)) {
 			System.out.println("Start game");
-
-
-			System.out.println("Enter Number of rows: ");
-			int a = in.nextInt();
-
+			System.out.println("Enter Number of rows: ");	        
+		        int a = in.nextInt();
 			System.out.println("Enter Number of columns: ");
 			int b = in.nextInt();
 			BuildMatrix startGame = new BuildMatrix();
